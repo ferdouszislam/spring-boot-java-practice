f067605475701c4e2da6566c2f2b92f5dab0ac0c
Ferdous Islam <ferdouszislam@gmail.com>
2022-03-16 10:44:00 +0600

    create spring boot project

---
+++ simplecrud/src/test/java/com/nsl/trainning/simplecrud/SimplecrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsl.trainning.simplecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplecrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
